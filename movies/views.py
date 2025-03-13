0e417655e0a2abc72b336971773b6268fe943f0e
Egor Popov <popoveg18@gmail.com>
2025-03-13 21:42:11 +0300

    Add pagination

--- movies/views.py
+++ movies/views.py
@@ -1,17 +1,25 @@
+from django.core import paginator
 from django.shortcuts import render
 
 from movies.models import Genre, Movie
+from django.core.paginator import Paginator
 
 def movies(request):
     """The movies view."""
+    
+    page = request.GET.get('page', 1)
+    
     movies = Movie.objects.all()
     genres = Genre.objects.all().order_by('-id')
+    
+    paginator = Paginator(movies, 4)
+    current_page = paginator.page(int(page))
     context = {
         'title': 'Фильмы - Filmora',
         'heading': 'Библиотека фильмов',
         'text': 'Здесь расположены фильмы которые вы смотрели.',
-        'movies': movies,
-        'genres': genres
+        'movies': current_page,
+        'genres': genres,
     }
     return render(request, 'movies/movies.html', context)
 
