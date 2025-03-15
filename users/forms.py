24aedb5019696b96c6d5c8cf24f5069e128746b3
Egor Popov <popoveg18@gmail.com>
2025-03-15 22:47:08 +0300

    Create relationship between form and html

--- users/forms.py
+++ users/forms.py
@@ -1,20 +1,13 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
     
-    username = forms.CharField(
-        label='Имя пользователя',
-        widget=forms.TextInput(attrs={"autofocus": True,
-                                                             "id": 'username'}))
-    password = forms.CharField(
-        label='Пароль',
-        widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
-                                          'id': 'password'}),
-    )
+    username = forms.CharField()
+    password = forms.CharField()
     
     class Meta:
         model = User
