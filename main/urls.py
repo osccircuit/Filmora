ce92f991238b703e9f42498ebf0a0be32d0a046f
Egor Popov <popoveg18@gmail.com>
2025-03-30 15:24:21 +0300

    Change index control on index class control

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,6 @@ from main import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('about/', views.about, name='about')
 ]
--- main/views.py
+++ main/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import redirect, render
+from django.views.generic import TemplateView
 
-def index(request):
-    """Main page of the site."""
-    if request.user.is_authenticated:
-        return redirect('movies:films')
-    my_context = {
-        'title': 'Главная - Filmora',
-        'heading': 'Добро пожаловать в Filmora',
-        'text': 'Filmora - это приложение, которое позволяет вам управлять вашей личной библиотекой фильмов. Вы можете добавлять, редактировать и удалять фильмы, а также просматривать информацию о них.',
-    }
-    return render(request, 'main/index.html', my_context)
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
+    
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('movies:films')
+        return super().get(request, *args, **kwargs)
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная - Filmora'
+        context['heading'] = 'Добро пожаловать в Filmora'
+        context['text'] = 'Filmora - это приложение, которое позволяет вам управлять вашей личной библиотекой фильмов. Вы можете добавлять, редактировать и удалять фильмы, а также просматривать информацию о них.'
+        return context
+
 
 def about(request):
     """About page of the site."""
