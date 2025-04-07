3971c872a02bc7bd59cc35e54fb21e615616284e
Egor Popov <popoveg18@gmail.com>
2025-04-07 23:02:55 +0300

    Add notification when mark or review is empty

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,7 +4,7 @@ from reviews import views
 app_name = 'reviews'
 
 urlpatterns = [
-    path('add-review/', views.add_review, name='add_review'),
+    path('add-review/', views.CreatorReviewView.as_view(), name='add_review'),
     path('delete-review/', views.delete_review, name='delete_review'),
     path('add-mark/', views.add_mark, name='add_mark'),
     path('delete-mark/', views.delete_mark, name='delete_mark')
--- reviews/views.py
+++ reviews/views.py
@@ -9,11 +9,68 @@ from reviews.forms import ReviewForm
 from users.models import UserMovie
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-class AddReview(LoginRequiredMixin, View):
+class CreatorReviewView(LoginRequiredMixin, View):
+    """_summary_
+
+    Args:
+        LoginRequiredMixin (_type_): _description_
+        View (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    paginate_by = 3
+    page = 1
     
     def post(self, request):
-        pass
-        
+        """_summary_
+
+        Args:
+            request (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        movie_id = request.POST.get("movie_id")
+        form_data = {
+            "review": request.POST.get("review"),
+            "mark": request.POST.get("mark"),
+        }
+        if form_data['review'] == '':
+            return JsonResponse({"error": "Отзыв не может быть пустым"}, status=400)
+        if form_data['mark'] is None:
+            return JsonResponse({"error": "Оценку все же нужно поставить"}, status=400)
+
+        movie = UserMovie.objects.filter(movie__id=movie_id).select_related('movie')
+        user_movie = movie.filter(user=request.user).first()
+
+
+        if user_movie.review is None:
+            user_movie.review = form_data["review"]
+            user_movie.mark = form_data["mark"]
+            user_movie.save()
+
+            paginator = Paginator(movie, self.paginate_by)
+
+            review_handler = render_to_string(
+                "includes/add_review.html",
+                {"movie": user_movie.movie, "review_not_add": False},
+                request,
+            )
+
+            users_reviews = render_to_string(
+                "includes/view_reviews.html",
+                {"reviews": paginator.page(self.page)},
+                request
+            )
+
+            response_data = {
+                "message": "Рецензия успешно опубликована.",
+                "review_handler": review_handler,
+                "users_reviews": users_reviews,
+            }
+            return JsonResponse(response_data)
+        return JsonResponse({"error": "Отзыв или оценка уже есть"}, status=400)
 
 def add_review(request):
     """Add user review"""
