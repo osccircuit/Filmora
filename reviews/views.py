8b3a65655306612d06fd7a3649e1c1d6b45f52af
Egor Popov <popoveg18@gmail.com>
2025-04-08 21:50:41 +0300

    Refactoring review view file

--- reviews/views.py
+++ reviews/views.py
@@ -1,13 +1,10 @@
 from django.core.paginator import Paginator
 from django.http import JsonResponse
-from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
 from django.views import View
-
-import reviews
-from reviews.forms import ReviewForm
-from users.models import UserMovie
 from django.contrib.auth.mixins import LoginRequiredMixin
+from users.models import UserMovie
+
 
 class CreatorReviewView(LoginRequiredMixin, View):
     """_summary_
@@ -19,9 +16,10 @@ class CreatorReviewView(LoginRequiredMixin, View):
     Returns:
         _type_: _description_
     """
+
     paginate_by = 3
     page = 1
-    
+
     def post(self, request):
         """_summary_
 
@@ -36,14 +34,13 @@ class CreatorReviewView(LoginRequiredMixin, View):
             "review": request.POST.get("review"),
             "mark": request.POST.get("mark"),
         }
-        if form_data['review'] == '':
-            return JsonResponse({"error": "Отзыв не может быть пустым"}, status=400)
-        if form_data['mark'] is None:
+        if form_data["review"] == "":
+            return JsonResponse({"error": "Отзыв придется написать"}, status=400)
+        if form_data["mark"] is None:
             return JsonResponse({"error": "Оценку все же нужно поставить"}, status=400)
 
-        movie = UserMovie.objects.filter(movie__id=movie_id).select_related('movie')
+        movie = UserMovie.objects.filter(movie__id=movie_id).select_related("movie")
         user_movie = movie.filter(user=request.user).first()
-
 
         if user_movie.review is None:
             user_movie.review = form_data["review"]
@@ -61,7 +58,7 @@ class CreatorReviewView(LoginRequiredMixin, View):
             users_reviews = render_to_string(
                 "includes/view_reviews.html",
                 {"reviews": paginator.page(self.page)},
-                request
+                request,
             )
 
             response_data = {
@@ -71,66 +68,3 @@ class CreatorReviewView(LoginRequiredMixin, View):
             }
             return JsonResponse(response_data)
         return JsonResponse({"error": "Отзыв или оценка уже есть"}, status=400)
-
-def add_review(request):
-    """Add user review"""
-
-    page = request.GET.get("page", 1)
-
-    if not request.user.is_authenticated:
-        return redirect("main:index")
-
-    movie_id = request.POST.get("movie_id")
-    form_data = {
-        "review": request.POST.get("review"),
-        "mark": request.POST.get("mark"),
-    }
-
-    user_movie = UserMovie.objects.filter(user=request.user, movie=movie_id).first()
-
-    all_reviews = (
-        UserMovie.objects.filter(movie=movie_id)
-        .select_related("user")
-        .values_list("user__username", "user__image", "added_at", "review", "mark")
-        .all()
-    )
-
-    n_reviews = UserMovie.objects.filter(movie__id=movie_id).select_related("user")
-
-    paginator = Paginator(n_reviews, 3)
-    current_page = paginator.page(int(page))
-
-    if user_movie.review is None or user_movie.review == "":
-        user_movie.review = form_data["review"]
-        user_movie.mark = form_data["mark"]
-        user_movie.save()
-
-        review_handler = render_to_string(
-            "includes/add_review.html",
-            {"movie": user_movie.movie, "review_not_add": "full"},
-            request,
-        )
-
-        users_reviews = render_to_string(
-            "includes/view_reviews.html", {"reviews": current_page}, request
-        )
-
-        response_data = {
-            "message": "Рецензия успешно опубликована.",
-            "review_handler": review_handler,
-            "users_reviews": users_reviews,
-        }
-        return JsonResponse(response_data)
-    return JsonResponse({"error": "Отзыв или оценка уже есть"}, status=400)
-
-
-def delete_review():
-    pass
-
-
-def add_mark():
-    pass
-
-
-def delete_mark():
-    pass
