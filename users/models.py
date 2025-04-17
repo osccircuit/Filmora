ff0853674c3211d925d928ea86cb38737c9d7f82
Egor Popov <popoveg18@gmail.com>
2025-04-17 22:21:18 +0300

    Add attributes for sub

--- users/models.py
+++ users/models.py
@@ -3,20 +3,41 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-
 class User(AbstractUser):
     """Refactor user model"""
-    image = models.ImageField(upload_to='users_images',
-                              blank=True, null=True,
-                              verbose_name='Аватар')
-    class Meta():
+
+    class TypeOfSub:
+        subs = {"FR": 'FREE', "ST": "STANDARD", "PR": "PREMIUM"}
+
+        @classmethod
+        def get_sub_choices(cls):
+            return cls.subs
+
+        @classmethod
+        def get_default_choice(cls):
+            return cls.subs['FR']
+
+    image = models.ImageField(
+        upload_to="users_images", blank=True, null=True, verbose_name="Аватар"
+    )
+    date_to_pay = models.DateField(blank=True, null=True, verbose_name="Дата оплаты")
+    type_of_sub = models.CharField(
+        max_length=8,
+        default=TypeOfSub.get_default_choice(),
+        choices=TypeOfSub.get_sub_choices(),
+        verbose_name="Тип подписки",
+    )
+
+    class Meta:
         """Meta class for User model."""
-        db_table = 'user'
-        verbose_name = 'Пользователя'
-        verbose_name_plural = 'Пользователи'
-        
+
+        db_table = "user"
+        verbose_name = "Пользователя"
+        verbose_name_plural = "Пользователи"
+
     def __str__(self):
         return str(self.username).capitalize()
+
 
 class UserMovie(models.Model):
     class RatingEnum(models.IntegerChoices):
@@ -25,18 +46,27 @@ class UserMovie(models.Model):
         THREE = 3
         FOUR = 4
         FIVE = 5
-    
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_movies')
-    movie = models.ForeignKey('movies.Movie', on_delete=models.CASCADE, related_name='movie_users')
-    review = models.TextField(null=True, blank=True, max_length=10000, verbose_name='Отзыв')
-    mark = models.IntegerField(choices=RatingEnum, null=True, blank=True, validators=[MinValueValidator(1), MaxValueValidator(5)], verbose_name='Оценка')
-    added_at = models.DateTimeField(auto_now_add=True, verbose_name='Время добавления')
 
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_movies")
+    movie = models.ForeignKey(
+        "movies.Movie", on_delete=models.CASCADE, related_name="movie_users"
+    )
+    review = models.TextField(
+        null=True, blank=True, max_length=10000, verbose_name="Отзыв"
+    )
+    mark = models.IntegerField(
+        choices=RatingEnum,
+        null=True,
+        blank=True,
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        verbose_name="Оценка",
+    )
+    added_at = models.DateTimeField(auto_now_add=True, verbose_name="Время добавления")
+
     class Meta:
-        unique_together = ('user', 'movie')
-        verbose_name = 'Фильм пользователя'
-        verbose_name_plural = 'Фильмы пользователя'
+        unique_together = ("user", "movie")
+        verbose_name = "Фильм пользователя"
+        verbose_name_plural = "Фильмы пользователя"
 
     def __str__(self):
-        return f'{self.user.username} - {self.movie.title}'
+        return f"{self.user.username} - {self.movie.title}"
