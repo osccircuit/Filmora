f8059a7d29531824483a3d87b171cff22ac78301
Egor Popov <popoveg18@gmail.com>
2025-04-22 22:45:31 +0300

    Use session id for add sub level

--- subscription/views.py
+++ subscription/views.py
@@ -17,13 +17,13 @@ class OrderSubscriptionView(LoginRequiredMixin, TemplateView):
 class PaySubscription(LoginRequiredMixin, View):
 
     def post(self, request):
-        subscription_level = request.COOKIES.get('subscription_level')
-        response = JsonResponse({
+        subscription_level = request.session.get('pending_sub')
+        del self.request.session['pending_sub']
+        response = {
             "message": f"Подписка {subscription_level} успешно оплачена вы будете перенаправлены на главную страницу через 5 секунд",
             "redirect_url": reverse("library:films"),
-        })
-        response.delete_cookie('subscription_level')
-        return response
+        }
+        return JsonResponse(response)
 
 class VariantSubscriptionView(LoginRequiredMixin, TemplateView):
     template_name = "subscription/variant_sub.html"
@@ -31,16 +31,11 @@ class VariantSubscriptionView(LoginRequiredMixin, TemplateView):
 
     def post(self, request, *args, **kwargs):
         self.sub_level = request.POST.get('level_sub')
-        response = JsonResponse({
+        self.request.session['pending_sub'] = self.sub_level
+        response = {
             "redirect_url": reverse("subscription:subscription_registration"),
-        })
-        response.set_cookie(
-           key="subscription_level",    # Название куки
-            value=self.sub_level,       # Значение
-            max_age=3600,               # Время жизни (1 час)
-            samesite='Lax'
-        )
-        return response
+        }
+        return JsonResponse(response)
         
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
