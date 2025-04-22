d3bfd258d7b3fec5b5e1c9cdf284bd22ee664c52
Egor Popov <popoveg18@gmail.com>
2025-04-22 23:14:13 +0300

    Limited access for pay web-page

--- subscription/views.py
+++ subscription/views.py
@@ -8,6 +8,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class OrderSubscriptionView(LoginRequiredMixin, TemplateView):
     template_name = "subscription/subscription.html"
 
+    def dispatch(self, request, *args, **kwargs):
+        access_status = request.session.get('allow_access')
+        if not access_status:
+            return HttpResponseRedirect(reverse("library:films"))
+        del self.request.session['allow_access']
+        return super().dispatch(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = "Оформление подписки"
@@ -18,6 +25,7 @@ class PaySubscription(LoginRequiredMixin, View):
 
     def post(self, request):
         subscription_level = request.session.get('pending_sub')
+        
         del self.request.session['pending_sub']
         response = {
             "message": f"Подписка {subscription_level} успешно оплачена вы будете перенаправлены на главную страницу через 5 секунд",
@@ -31,6 +39,7 @@ class VariantSubscriptionView(LoginRequiredMixin, TemplateView):
     def post(self, request, *args, **kwargs):
         sub_level = request.POST.get('level_sub')
         self.request.session['pending_sub'] = sub_level
+        self.request.session['allow_access'] = True 
         response = {
             "redirect_url": reverse("subscription:subscription_registration"),
         }
