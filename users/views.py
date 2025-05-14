d37d89bce76c35f76ec905508dc1cef4796c6d2f
Egor Popov <popoveg18@gmail.com>
2025-05-14 22:01:43 +0300

    Fixed registration context function

--- users/views.py
+++ users/views.py
@@ -112,6 +112,7 @@ class RegistrationView(CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = "Регистрация - Filmora"
+        return context
 
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
